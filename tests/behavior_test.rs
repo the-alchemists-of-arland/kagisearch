db7591123d294d4d2f8f637272a4782ee7b8d386
Hanchin Hsieh <me@yuchanns.xyz>
2025-03-24 15:49:07 +0800

    refactor: drop outdated playwright-rust and replace by chromiumoxide
    Hanchin Hsieh <me@yuchanns.xyz>

--- tests/behavior_test.rs
+++ tests/behavior_test.rs
@@ -1,10 +1,21 @@
-use kagisearch::{AuthType, Browser};
+use kagisearch::{AuthType, Kagi, Spawner};
+
+struct TokioSpawner;
+
+impl Spawner for TokioSpawner {
+    fn spawn(future: impl std::future::Future<Output = ()> + Send + 'static) {
+        tokio::spawn(future);
+    }
+}
 
 #[tokio::test]
 async fn test_search() -> anyhow::Result<()> {
     let token = std::env::var("KAGI_TOKEN")?;
-    let browser = Browser::new(AuthType::Token(token)).await?;
-    let results = browser.search("Rust programming language", 5).await?;
+    let mut kagi = Kagi::new::<TokioSpawner>(AuthType::Token(token)).await?;
+    let results = kagi.search("Rust programming language", 5).await?;
+    let Some(results) = results else {
+        return Err(anyhow::anyhow!("No search results found"));
+    };
 
     assert_eq!(results.len(), 5);
     for result in results {
@@ -12,6 +23,7 @@ async fn test_search() -> anyhow::Result<()> {
         assert!(!result.url.is_empty());
         assert!(!result.snippet.is_empty());
     }
+    kagi.close().await?;
 
     Ok(())
 }
